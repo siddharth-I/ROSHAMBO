479df4243d9347d3ae844490c0fc9b46957b8517
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2020-09-14 18:23:42 -0400

    Add files via upload

---
+++ ROSHAMBO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ROSHAMBO
+{
+    public class ROSHAMBO
+    {
+        public string StartGame(string inputOne, string inputTwo)
+        {
+            Console.WriteLine("What is your first play?");
+            inputOne = Console.ReadLine();
+            Console.WriteLine("What is your second play?");
+            inputTwo = Console.ReadLine();
+            if (inputOne == "Rock" && inputTwo == "Paper") || (inputOne == "Paper" && inputTwo == "Rock");
+            {
+                Console.WriteLine("Paper covers Rock");
+            }
+            else if (inputOne == "Scissors" && )
+        }
+    }
+}
